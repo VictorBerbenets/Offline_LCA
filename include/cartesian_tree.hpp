e15d9c01ff91bff6de452447442b18125e42eca7
Victor Berbenets <berbenets.vitia@yandex.ru>
2024-03-10 17:50:18 +0300

    making Node class for cartesian tree

--- include/cartesian_tree.hpp
+++ include/cartesian_tree.hpp
@@ -1,1 +1,29 @@
 
+
+namespace yLAB {
+
+template <typename> class Treap;
+
+namespace detail {
+
+template <typename Key, typename Priority>
+class Node {
+ public:
+  using key_type      = Key;
+  using priority_type = Priority;
+
+  template <typename T> friend class Treap;
+ private:
+  key_type key_;
+  priority_type priority_;
+};
+
+template <typename T>
+class Treap {
+
+};
+
+} // <--- namespace detail
+
+} // <--- namespace yLAB
+
