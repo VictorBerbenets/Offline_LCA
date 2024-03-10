34fd600c85e9c2bb05d2ecefcb2a2b7b81360a7d
Victor Berbenets <berbenets.vitia@yandex.ru>
2024-03-10 18:10:13 +0300

    making a cartesian tree

--- include/cartesian_tree.hpp
+++ include/cartesian_tree.hpp
@@ -1,4 +1,7 @@
-
+#include <memory>
+#include <vector>
+#include <iterator>
+#include <cstddef>
 
 namespace yLAB {
 
@@ -31,7 +34,25 @@ class Node {
 
 template <typename T>
 class Treap {
+ public:
+  using size_type       = std::size_t;
+  using key_type        = size_type;
+  using value_type      = T;
+  using difference_type = std::ptrdiff_t;
+  using reference       = value_type&;
+  using const_reference = const value_type&;
 
+ private:
+  using node_type = detail::Node<key_type, value_type>;
+ public:
+  template <std::input_iterator Iter>
+  Treap(Iter begin, Iter end) {
+
+  }
+
+ private:
+  node_type *root_;
+  std::vector<std::unique_ptr<node_type>> storage_;
 };
 
 } // <--- namespace yLAB
