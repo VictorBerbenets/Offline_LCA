cd7fd1a381844b32f555ab68d9ac2fdb984fb9f6
Victor Berbenets <berbenets.vitia@yandex.ru>
2024-03-10 17:58:47 +0300

    added new fields and constructor for Node class

--- include/cartesian_tree.hpp
+++ include/cartesian_tree.hpp
@@ -11,11 +11,19 @@ class Node {
  public:
   using key_type      = Key;
   using priority_type = Priority;
+  
+  Node(const key_type &key, const priority_type &priority, Node *right = nullptr,
+       Node *left = nullptr, Node *parent = nullptr)
+      : key_ {key}, priority_ {priority_},
+        left_ {left}, right_ {right},
+        parent_ {parent} {}
 
   template <typename T> friend class Treap;
  private:
   key_type key_;
   priority_type priority_;
+
+  Node *left_, *right_, *parent_;
 };
 
 template <typename T>
