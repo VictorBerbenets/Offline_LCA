9c529f9037b1605ebe5ba88177759fe8ffcf4e39
Victor Berbenets <berbenets.vitia@yandex.ru>
2024-03-11 11:41:20 +0300

    changed pointer type on base_node*

--- include/iterator.hpp
+++ include/iterator.hpp
@@ -12,50 +12,55 @@ template <typename> class Treap;
 
 template<typename KeyT, typename Priority>
 class TreeIterator final {
-public:
-    using iterator_category = std::bidirectional_iterator_tag;
-    using value_type        = detail::Node<KeyT, Priority>;
-    using pointer           = value_type*;
-    using reference         = value_type&;
-    using const_pointer     = const value_type*;
-    using const_reference   = const value_type&;
-    using difference_type   = std::ptrdiff_t;
+  using node_type = detail::Node<KeyT, Priority>;
+ public:
+  using iterator_category = std::bidirectional_iterator_tag;
+  using value_type        = detail::BaseNode<KeyT, Priority>;
+  using pointer           = value_type*;
+  using reference         = value_type&;
+  using const_pointer     = const value_type*;
+  using const_reference   = const value_type&;
+  using difference_type   = std::ptrdiff_t;
 
-    TreeIterator() = default;
+  TreeIterator() = default;
 
-    TreeIterator& operator++() {
-        ptr_ = ptr_->successor();
-        return *this;
-    }
+  TreeIterator& operator++() {
+    ptr_ = static_cast<node_type*>(ptr_)->successor();
+    return *this;
+  }
 
-    TreeIterator& operator--() {
-        ptr_ = ptr_->predecessor();
-        return *this;
-    }
+  TreeIterator& operator--() {
+    ptr_ = static_cast<node_type*>(ptr_)->predecessor();
+    return *this;
+  }
 
-    TreeIterator operator++(int n) {
-        auto tmp = *this;
-        ++(*this);
-        return tmp;
-    }
+  TreeIterator operator++(int n) {
+    auto tmp = *this;
+    ++(*this);
+    return tmp;
+  }
 
-    TreeIterator operator--(int n) {
-        auto tmp = *this;
-        --(*this);
-        return tmp;
-    }
+  TreeIterator operator--(int n) {
+    auto tmp = *this;
+    --(*this);
+    return tmp;
+  }
 
-    const KeyT& operator*() const noexcept { return ptr_->key_; }
-    const KeyT* operator->() const noexcept { return std::addressof(ptr_->key_); }
+  const Priority& operator*() const noexcept {
+    return static_cast<node_type*>(ptr_)->priority_;
+  }
+  const Priority* operator->() const noexcept {
+    return std::addressof(static_cast<node_type*>(ptr_)->priority_);
+  }
 
-    auto operator<=>(const TreeIterator&) const = default;
+  auto operator<=>(const TreeIterator&) const = default;
 
-    template<typename> friend class Treap;
-private:
+  template<typename> friend class Treap;
+ private:
 /*----------------------------------------------------------------------------------*/
-    pointer ptr_ {nullptr};
+  pointer ptr_ {nullptr};
 
-    TreeIterator(pointer ptr): ptr_ {ptr} {}
+  TreeIterator(pointer ptr): ptr_ {ptr} {}
 };
 
 } // <--- namespace yLAB
