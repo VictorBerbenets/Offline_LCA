49ee19f86e1d6a7814e9c6b6a8a6b76f5924882a
Victor Berbenets <berbenets.vitia@yandex.ru>
2024-03-16 14:59:26 +0300

    many overwrittens and fixed bugs

--- include/rmq.hpp
+++ include/rmq.hpp
@@ -6,6 +6,7 @@
 #include <utility>
 #include <climits>
 #include <bitset>
+#include <initializer_list>
 
 #include "cartesian_tree.hpp"
 #include "sparse_table.hpp"
@@ -13,7 +14,7 @@
 namespace yLAB {
 
 template <typename T>
-class RmqSolver final {
+class RmqSolver final: protected SparseTable<std::size_t> {
  public:
   using value_type  = T;
   using size_type   = std::size_t;
@@ -21,31 +22,50 @@ class RmqSolver final {
   using tree_type   = Treap<value_type>;
   using sq_table    = std::vector<std::vector<size_type>>;
   using block_bits  = std::bitset<sizeof(int) * CHAR_BIT>;
+
+  using sparse_table = SparseTable<size_type>;
+  using sparse_table::sparse_;
  public:
+ 
+  RmqSolver(std::initializer_list<value_type> i_list)
+      : RmqSolver(i_list.begin(), i_list.end()) {}
 
   template <std::input_iterator Iter>
   RmqSolver(Iter begin, Iter end) {
     euler_tour(begin, end);
     rmq_plus_minus_1();
   }
-  
-  value_type ans_query(const std::pair<size_type, size_type> &query) const {
+
+  value_type ans_query(const std::pair<size_type, size_type> &query) {
     auto [left_id, right_id] = get_heights_positions(query);
-    auto fst_block = left_id / block_sz_, sec_block = right_id / block_sz_;
-
-    auto left_min   = sections_mins_[block_types_[fst_block]][left_id % block_sz_][block_sz_ - 1];
-    auto right_min  = sections_mins_[block_types_[sec_block]][0][right_id % block_sz_];
-    if (fst_block != sec_block) {
-      auto middle_min = sparse_table_[fst_block][sec_block];
-
-      return std::min({get_val(left_min), get_val(right_min), get_val(middle_min)});
+    if (left_id > right_id) {
+      std::swap(left_id, right_id);
     }
-    auto block_min = sections_mins_[block_types_[fst_block]][left_min][right_min];
-
-    return get_val(block_min);
+    return euler_tour_[rmq({left_id, right_id})];
   }
 
  private:
+  size_type rmq(const std::pair<size_type, size_type> &query) const {
+   auto left_block  = query.first / block_sz_;
+   auto right_block = query.second / block_sz_;
+   // if both indexes are inside the same block
+   if (left_block == right_block) {
+     return block_rmq(left_block, query.first % block_sz_, query.second % block_sz_);
+   }
+   // find the minimum on the segment from l to the end of the block containing l
+   auto ansl = block_rmq(left_block, query.first % block_sz_, block_sz_ - 1);
+   // find the minimum from the beginning of the block containing r to r 
+   auto ansr = block_rmq(right_block, 0, query.second % block_sz_);
+   // find the minimum on the blocks between the outer ones, if there are any
+   if (left_block + 1 < right_block) {
+     auto power = log2_floor(right_block - left_block - 1);
+     auto ansb  = min(sparse_[left_block + 1][power],
+                      sparse_[right_block - (1 << power)][power]);
+     return min(ansb, min(ansl, ansr));
+   }
+   return min(ansl, ansr);
+  }
+
   template <std::input_iterator Iter>
   void euler_tour(Iter begin, Iter end) {
     tree_type tree (begin, end);
@@ -94,42 +114,45 @@ class RmqSolver final {
     if (auto log = log2_floor(heights_.size()); log > 2) {
       block_sz_ = log / 2; 
     }
-
-    build_sparse_table();
-    precount_minimums_in_blocks();
+    
+    auto min_blocks_pos = get_min_pos_in_each_block();
+    build_sparse_table(min_blocks_pos.begin(), min_blocks_pos.end(),
+                       min_blocks_pos.size());
+    precompute_all_blocks_rmq();
+    compute_each_block_type();
   }
  
-  void build_sparse_table() {
+  std::vector<int> get_min_pos_in_each_block() {
     size_type size = heights_.size();
     size_type blocks_num = size / block_sz_ + (size % block_sz_ ? 1 : 0);
-
-    std::vector<size_type> blocks_mins;
-    blocks_mins.reserve(blocks_num);
-    block_types_.reserve(blocks_num);
-    size_type block_id = 0, block_num = 0, type = 0, min = heights_[0];
-    for (size_type id = 0; id < size; ++id, ++block_id) {
-      if (block_id < block_sz_) {
-        if (heights_[id] < min) {
-          min = heights_[id];
-        }
-        if (block_id + 1 < block_sz_ && id + 1 < size) {
-          type += heights_[id] < heights_[id + 1] ? 1 << block_id : 0;
-        }
-      } else if (block_id == block_sz_) {
-        blocks_mins.push_back(std::exchange(min, heights_[id]));
-        block_types_[block_num++] = type;
-        block_id = 0;
+    block_types_.assign(blocks_num, 0);
+    std::vector<int> blocks_mins(blocks_num, -1);
+    int curr_block = -1;
+    for (size_type id = 0; id < size; ++id) {
+      if (id % block_sz_ == 0) {
+        ++curr_block;
+      }
+      if (blocks_mins[curr_block] == -1 ||
+          heights_[blocks_mins[curr_block]] > heights_[id]) {
+        blocks_mins[curr_block] = id;
       }
     }
-    if (block_id != block_sz_ + 1) {
-      blocks_mins.push_back(min);
-      block_types_[block_num] = type;
+    return blocks_mins;
+  }
+ 
+  void compute_each_block_type() {
+    for (size_type i = 0, j = 0, curr_block = 0, size = heights_.size();
+         i < size || j < block_sz_; ++i, ++j) {
+      if (j >= block_sz_) {
+        j = 0, ++curr_block;
+      }
+      if (j > 0 && (i >= size || heights_[i - 1] < heights_[i])) {
+        block_types_[curr_block] += (1 << (j - 1));
+      }
     }
-    sparse_table_.construct(blocks_mins.begin(), blocks_mins.end(),
-                            blocks_mins.size());
   }
 
-  void precount_minimums_in_blocks() {
+  void precompute_all_blocks_rmq() {
     // we have 2^(block_sz - 1)  different blocks
     size_type diff_blocks = 1 << (block_sz_ - 1);
     sections_mins_.assign(diff_blocks, sq_table(block_sz_,
@@ -150,10 +173,34 @@ class RmqSolver final {
       }
     }
   }
+  
+  template <std::input_iterator Iter>
+  void build_sparse_table(Iter begin, Iter end, size_type size) {
+    size_type log = log2_floor(heights_.size());
+    sparse_.resize(size, std::vector<size_type>(log + 1));
+ 
+    for (int i = 0; begin != end; ++begin, ++i) {
+      sparse_[i][0] = *begin;
+    }
+    for (size_type j = 1; j <= log; ++j) {
+      for (size_type i = 0; i < size; ++i) {
+        size_type ind = (1 << (j - 1)) + i;
+        if (ind >= size) {
+          sparse_[i][j] = sparse_[i][j - 1];
+        }
+        else if (heights_[sparse_[i][j - 1]] > heights_[sparse_[ind][j - 1]]) {
+          sparse_[i][j] = sparse_[ind][j - 1];
+        }
+        else {
+          sparse_[i][j] = sparse_[i][j - 1];
+        }
+      }
+    }
+  }
 
-  std::vector<int> get_block_section(size_type block_id) const noexcept {
+  std::vector<int> get_block_section(size_type block_id) const {
     block_bits b_set(block_id);
-    std::vector<int> section(block_sz_, 0);
+    std::vector section(block_sz_, 0);
     int assign = 0;
     for (size_type i = 1; i < block_sz_; ++i) {
       if (b_set[i - 1] == 0) {
@@ -165,13 +212,18 @@ class RmqSolver final {
     return section;
   }
  
-  std::pair<size_type, size_type>
-  get_heights_positions(const std::pair<size_type, size_type> &query) const {
-    return std::make_pair(heights_[query.first], heights_[query.second]);
+  size_type min(size_type l, size_type r) const {
+    return heights_[l] < heights_[r] ? l : r;
   }
 
-  value_type get_val(size_type height_id) const {
-    return euler_tour_[heights_[height_id]]; 
+  size_type block_rmq(size_type block_num, size_type l, size_type r) const {
+    return sections_mins_[block_types_[block_num]][l][r] + block_num * block_sz_;
+  }
+ 
+  std::pair<size_type, size_type>
+  get_heights_positions(const std::pair<size_type, size_type> &query) const {
+    return std::make_pair(first_appear_[query.first],
+                          first_appear_[query.second]);
   }
 
  private:
@@ -180,9 +232,11 @@ class RmqSolver final {
   std::vector<size_type> heights_;
   std::vector<size_type> block_types_;
   std::vector<sq_table>  sections_mins_;
-  SparseTable<value_type> sparse_table_;
   size_type block_sz_ {1};
 };
+
+template <std::input_iterator Iter>
+RmqSolver(Iter, Iter) -> RmqSolver<typename std::iterator_traits<Iter>::value_type>;
 
 } // <--- namespace yLAB
 
