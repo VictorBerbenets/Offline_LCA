7d3ffdf28302210cf6397e55dc13d3a9544af167
Victor Berbenets <berbenets.vitia@yandex.ru>
2024-03-17 12:10:45 +0300

    wrote end2end tests

--- tests/end2end/include/end2end.hpp
+++ tests/end2end/include/end2end.hpp
@@ -25,36 +25,36 @@ class generator final {
   using generator_type    = std::mt19937;
   using distribution_type = std::uniform_int_distribution<T>;
   
-  static constexpr int64_t MIN_VALUE                = -1000000;
-  static constexpr int64_t MAX_VALUE                = 1000000;
-  static constexpr size_type DEFAULT_SIZE           = 1000000;
+  static constexpr int64_t MIN_VALUE                = 0;
+  static constexpr int64_t MAX_VALUE                = 100000000;
+  static constexpr size_type DEFAULT_SIZE           = 10000000;
   static constexpr size_type DEFAULT_QUERIES_NUMBER = 1000000;
 
   template <std::integral U>
   U random_value(U min_val, U max_val) {
-      distribution_type distr(min_val, max_val);
-      return distr(generator_);
+    distribution_type distr(min_val, max_val);
+    return distr(generator_);
   }
 
   void create_source_directory() {
-      using namespace std::filesystem;
-      directory_entry resource_dir(dirs::resource_dir);
+    using namespace std::filesystem;
+    directory_entry resource_dir(dirs::resource_dir);
 
-      if (!resource_dir.is_directory()) {
-          create_directory(dirs::resource_dir);
-          create_directory(dirs::tests_dir);
-          create_directory(dirs::ans_dir);
-      } else {
-          const path tests_path{dirs::tests_dir};
-          const path answs_path{dirs::ans_dir};
+    if (!resource_dir.is_directory()) {
+      create_directory(dirs::resource_dir);
+      create_directory(dirs::tests_dir);
+      create_directory(dirs::ans_dir);
+    } else {
+      const path tests_path{dirs::tests_dir};
+      const path answs_path{dirs::ans_dir};
 
-          for (auto& dir_iter : directory_iterator{tests_path}) {
-              remove(dir_iter.path());
-          }
-          for (auto& dir_iter : directory_iterator{answs_path}) {
-              remove(dir_iter.path());
-          }
+      for (auto& dir_iter : directory_iterator{tests_path}) {
+          remove(dir_iter.path());
       }
+      for (auto& dir_iter : directory_iterator{answs_path}) {
+          remove(dir_iter.path());
+      }
+    }
   }
 
   void generate_array_and_queries(size_type test_number) { 
@@ -64,51 +64,54 @@ class generator final {
     std::ofstream ans_file  {dirs::ans_dir + ans_name};
     
     // generating array
+    array_size_ = (array_size_ == 0 ? 1 : array_size_);
     std::vector<int64_t> array(array_size_);
-    std::generate(array.begin(), array.end(), random_value(min_value_, max_value_));
+    std::generate(array.begin(), array.end(), [&] { return random_value(min_value_, max_value_); });
     for (auto &&v : array) {
       test_file << "k " << v << ' ';
     }
     // generating queries
     std::vector<std::pair<size_type, size_type>> queries(queries_num_);
-    std::generate(array.begin(), array.end(), [size = array.size()] {
-                       auto l = random_value(0, size);
-                       auto r = random_value(l, size);
+    std::generate(queries.begin(), queries.end(), [&, size = array.size()] {
+                       auto l = random_value(0ul, size - 1);
+                       auto r = random_value(l, size - 1);
                        return std::make_pair(l, r);
                   });
+    for (auto &&[l, r] : queries) {
+      test_file << "q " << l << ' ' << r << ' '; 
+    }
     // generating answers with sparse table
     yLAB::SparseTable<int64_t> sparse_table(array.begin(), array.end(), array.size());
     std::transform(queries.begin(), queries.end(), std::ostream_iterator<int64_t>(ans_file, " "),
                   [&sparse_table](auto &&pair) {
-      return  sparse_table.min(pair.first, pair.second); 
+      return  sparse_table.min({pair.first, pair.second}); 
     });
   }
 
-
  public:
-  generator(size_type tests_number, int64_t min = MIN_VALUE, int64_t max = MAX_VALUE,
-            size_type arr_sz = DEFAULT_SIZE, size_type queries = DEFAULT_QUERIES_NUMBER)
-      : tests_number_ {tests_number},
-        min_value_ {min},
-        max_value_ {max},
-        array_size_ {arr_sz},
-        queries_num_ {queries} {
+  generator(size_type tests_number)
+      : tests_number_ {tests_number} {
     generator_.seed(static_cast<size_type>(std::time(nullptr)));
   }
 
-  void generate_tests() {
+  void generate_tests(int64_t min = MIN_VALUE, int64_t max = MAX_VALUE,
+                      size_type arr_sz = DEFAULT_SIZE,
+                      size_type queries = DEFAULT_QUERIES_NUMBER) {
     create_source_directory();
-
+    min_value_ = min, max_value_ = max, array_size_ = arr_sz,
+    queries_num_ = queries;
     for (size_type counter = 1; counter <= tests_number_; ++counter) {
+      std::cout << counter << std::endl;
         generate_array_and_queries(counter);
     }
   }
+
  private:
   size_type tests_number_;
-  int64_t min_value_;
-  int64_t max_value_;
-  size_type array_size_;
-  size_type queries_num_;
+  int64_t min_value_     = MIN_VALUE;
+  int64_t max_value_     = MAX_VALUE;
+  size_type array_size_  = DEFAULT_SIZE;
+  size_type queries_num_ = DEFAULT_QUERIES_NUMBER;
   generator_type generator_;
 };
 
--- tests/end2end/src/main.cpp
+++ tests/end2end/src/main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <stdexcept>
+#include <string>
 
 #include "end2end.hpp"
 
-int main(int argc, char* argv[]) {
+namespace ts = testing;
 
+int main(int argc, char* argv[]) {
+  if (argc < 2 || argc > 6) {
+    throw std::runtime_error {"invalid set of arguments, expected 1 - 5, got" +
+                              std::to_string(argc - 1)};
+  }
+  
+  auto test_num = std::stoul(argv[1]);
+  ts::generator<int64_t> gen(test_num);
+  if (argc == 2) {
+    gen.generate_tests();
+  } else if (argc == 3) {
+    gen.generate_tests(std::stol(argv[2]));
+  } else if (argc == 4) {
+    gen.generate_tests(std::stol(argv[2]), std::stol(argv[3]));
+
+  } else if (argc == 5) {
+    gen.generate_tests(std::stol(argv[2]), std::stol(argv[3]),
+                       std::stoul(argv[4]));
+
+  } else if (argc == 6) {
+    gen.generate_tests(std::stol(argv[2]), std::stol(argv[3]),
+                       std::stoul(argv[4]), std::stoul(argv[5]));
+
+  }
 }
+
