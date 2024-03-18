43630263c979bc1caa73f4f80561ab3694d5297f
Victor Berbenets <berbenets.vitia@yandex.ru>
2024-03-18 15:20:06 +0300

    added input checking

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <stdexcept>
+#include <string>
 #include <vector>
 #include <fstream>
 
 #include "rmq.hpp"
 
 namespace {
+
+  void check_input(std::istream &is) {
+    if (is.fail()) {
+      throw std::runtime_error {"data reading error"};
+    }
+  }
 
   auto get_data(std::istream &is) { 
     std::vector<int64_t> array;
@@ -15,15 +23,19 @@ namespace {
     std::size_t l = 0, r = 0;
     for (; is.good(); ) {
       is >> atribute;
-      if (is.fail()) {
-        break;
-      }
+      check_input(is);
       if (atribute == 'k') {
         is >> arr_elem;
+        check_input(is);
         array.push_back(arr_elem);
       } else if (atribute == 'q') {
         is >> l >> r;
+        check_input(is);
         queries.push_back({l ,r});
+      }
+      else {
+        throw std::runtime_error {"invalid input atribute, expected k or q, got: " +
+                                  std::to_string(atribute)};
       }
     }
     return std::make_pair(array, queries);
